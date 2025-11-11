203aee0839024c70d54444a0c334dce865f439e5
Gabriel_Mendes <gh185353@gmail.com>
2025-11-11 10:58:54 -0300

    Update transform_data.py

--- src/domain/transform_data.py
+++ src/domain/transform_data.py
@@ -1,330 +1,142 @@
-import logging
-from abc import ABC, abstractmethod
-import yaml
-import requests
-from io import StringIO
-import csv
 from datetime import datetime
-from urllib.parse import urlencode
 
+from src.infra.fetch_data import (
+    SelicFetchData,
+    FocusFetchData,
+    DolarFetchData,
+    IbgeFetchData,
+    TesouroFetchData
+)
 
-# ============== Configuração de Log ==============
-logging.basicConfig(level=logging.INFO, format="%(levelname)s: %(message)s")
+class ColDtReferencia:
 
-# ============== Carregar arquivo de endpoints ==============
-with open("api_endpoints.yaml", "r") as file:
-    file_yaml = yaml.safe_load(file)
+    @staticmethod
+    def add_dt_referencia(data: list[dict], date: str) -> list[dict]:
+        for item in data:
+            item['dt_execucao'] = datetime.strptime(date, "%d/%m/%Y").date()
+        return data
 
+class TransformFocusData:
+    def __init__(self, indicators: list[str], date: str, temporal_series: str):
+        self.indicators = indicators
 
-# ============== Classe Base ==============
-class IFetchData(ABC):
-
-    def __init__(self, endpoint: str):
-        self.endpoint = endpoint
-
-    @abstractmethod
-    def parse_date(self, date_str: str) -> datetime: ...
-
-    @abstractmethod
-    def build_url(self) -> str: ...
-
-    @abstractmethod
-    def fetch_data(self) -> dict: ...
-
-
-# ============== API Selic ==============
-class SelicFetchData(IFetchData):
-    def __init__(self):
-        super().__init__(file_yaml["endpoints"]["bcb"]["selic"]["url"])
-
-    def parse_date(self, date_str: str) -> datetime:
-        """
-        Tenta interpretar a data em diferentes formatos possíveis.
-        Suporta os formatos: dd/mm/yyyy e yyyy-mm-dd.
-        """
-        for date_format in ("%d/%m/%Y", "%Y-%m-%d"):
-            try:
-                return datetime.strptime(date_str, date_format)
-            except ValueError:
-                pass
-        raise ValueError(f"Formato de data inválido: {date_str}")
-
-    def build_url(self, date: str) -> str:
-        """
-        Constroi a URL com os parâmetros necessários.
-        """
-        parsed_date = self.parse_date(date).strftime("%d/%m/%Y")
-        params = {
-            "formato": "json",
-            "dataInicial": parsed_date,
-            "dataFinal": parsed_date,
-        }
-
-        return f"{self.endpoint}?{urlencode(params)}"
-
-    def fetch_data(self, date: str) -> dict:
-        """ "
-        Busca os dados da API Selic.
-        """
-        try:
-            url = self.build_url(date)
-            logging.debug(f"URL Selic: {url}")
-            # Faz a requisição HTTP
-            r = requests.get(url, timeout=30)
-            r.raise_for_status()
-            return r.json()
-
-        except Exception as e:
-            logging.error(f"Erro ao buscar dados: {e}")
-            return None
-
-
-# ============== API Expectativa Mercado ==============
-class FocusFetchData(IFetchData):
-    """Classe para coletar dados de expectativas de mercado
-    da API Focus (BCB)."""
-
-    def __init__(self) -> None:
-        super().__init__(file_yaml["endpoints"]["bcb"]["focus"]["url"])
-
-    def parse_date(self, date_str: str) -> datetime:
-        """
-        Tenta interpretar a data em diferentes formatos possíveis.
-        Suporta os formatos: dd/mm/yyyy e yyyy-mm-dd.
-        """
-        for date_format in ("%d/%m/%Y", "%Y-%m-%d"):
-            try:
-                return datetime.strptime(date_str, date_format)
-            except ValueError:
-                pass
-        raise ValueError(f"Formato de data inválido: {date_str}")
-
-    def build_url(
-        self, indicator: str, date: str, temporal_series: str
-    ) -> str:
-        """Constrói URL completa para requisição da API Olinda."""
-
-        if indicator not in file_yaml["endpoints"]["bcb"]["focus"]["params"]["indicator"]["opcoes"]:
-            raise ValueError(f"Indicador inválido: {indicator}")
+        # Adicionar proximo ano na data
+        dt_date = datetime.strptime(date,"%d/%m/%Y").date()
+        self.date = [date,dt_date.replace(year = dt_date.year + 1).strftime("%d/%m/%Y")]
         
-        if not date or not indicator or not temporal_series:
-            raise ValueError(
-                "`dt_referencia`, `indicador` e `serie_temporal` "
-                "são obrigatórios para construir a URL."
-            )
+        self.temporal_series = temporal_series
 
-        if temporal_series == "anual":
-            date_str = self.parse_date(date).strftime("%Y")
+    def transform(self) -> list[dict]:
+        """
+        Transforma os dados da API Focus em um dicionário.
+        """
+        result_focus = []
 
-            url = (
-                f"{self.endpoint}ExpectativasMercadoAnuais?$format=json&"
-                f"$filter=Indicador eq '{indicator}' and "
-                f"DataReferencia eq '{date_str}'"
-            )
+        for indicator in self.indicators:
+            for date in self.date:
 
-        elif temporal_series == "mensal":
-            date_str = self.parse_date(date).strftime("%m/%Y")
+                fetch_data = FocusFetchData().fetch_data(indicator, date, self.temporal_series)
+                
+                dict_filtered = fetch_data[-1]
 
-            url = (
-                f"{self.endpoint}ExpectativaMercadoMensais?$format=json&"
-                f"$filter=Indicador eq '{indicator}' and "
-                f"DataReferencia eq '{date_str}'"
-            )
+                keys = ['Indicador','Data','DataReferencia','Mediana']
+
+                dict_filtered = {key: dict_filtered[key] for key in keys}
+
+                result_focus.append(dict_filtered)
+
+        result_focus = ColDtReferencia.add_dt_referencia(result_focus, self.date[0])
+
+        return result_focus
+
+class TransformIbgeData:
+    def __init__(self, date: str):
+        self.date = date
+
+    def transform(self) -> list[dict]:
+        """
+        Transforma os dados da API IBGE em um dicionário.
+        """
+
+        fetch_data = IbgeFetchData().fetch_data(self.date)
+
+        # Arrays de mapeamento para substituir os nomes das keys
+        keys = ['V', 'D2C', 'D3C', 'D3N']  
+        new_keys = ['valor_ipca', 'ano_mes', 'variavel_codigo', 'variavel_nome']
+        
+        # Converter nomes das keys
+        if isinstance(fetch_data, list):
+            # Itera pela lista e transforma cada item (dicionário)
+            data_transform = [
+                {new_keys[keys.index(k)]: v for k, v in item.items() if k in keys}
+                for item in fetch_data
+            ]
         else:
-            raise ValueError(
-                "`serie_temporal` inválido. Deve ser 'anual' ou 'mensal'."
-            )
+            raise TypeError("fetch_data precisa ser uma lista!")
+        
+        variaveis = ['63','2265']
+        result_ibge = [
+            {key: item[key] for key in new_keys}
+            for item in data_transform[1:]
+            if item.get('variavel_codigo') in variaveis
+        ]
 
-        return url
+        result_ibge = ColDtReferencia.add_dt_referencia(result_ibge, self.date)
 
-    def fetch_data(
-        self, indicator: str, date: str, temporal_series: str
-    ) -> dict:
+        return result_ibge
+
+class TransformSelicData:
+    def __init__(self, date: str):
+        self.date = date
+
+    def transform(self) -> list[dict]:
         """
-        Realiza a requisição HTTP e retorna os dados como um DataFrame.
+        Transforma os dados da API Selic em um dicionário.
         """
-        try:
-            url = self.build_url(indicator, date, temporal_series)
-            logging.debug(f"URL construída: {url}")
+        fetch_data = SelicFetchData().fetch_data(self.date)
 
-            # Faz a requisição HTTP
-            r = requests.get(url, timeout=30)
-            r.raise_for_status()
+        result_selic = ColDtReferencia.add_dt_referencia(fetch_data, self.date)
+        return result_selic
 
-            data = r.json().get("value", [])
+class TransformDolarData:
+    def __init__(self, date: str):
+        self.date = date
 
-            # Aplicar filtro de baseCalculo
-            data = [item for item in data if item["baseCalculo"] == 0]
-            return data
-
-        except Exception as e:
-            logging.error(f"Erro na API Boletim Focus: {e}")
-            return None
-
-
-# ============== API Dolar ==============
-class DolarFetchData(IFetchData):
-    """Classe para coletar dados de dolar da API Dolar (BCB)."""
-
-    def __init__(self) -> None:
-        super().__init__(file_yaml["endpoints"]["bcb"]["dolar"]["url"])
-
-    def parse_date(self, date_str: str) -> datetime:
+    def transform(self) -> list[dict]:
         """
-        Tenta interpretar a data em diferentes formatos possíveis.
-        Suporta os formatos: dd/mm/yyyy e yyyy-mm-dd.
+        Transforma os dados da API Selic em um dicionário.
         """
-        for date_format in ("%d/%m/%Y", "%Y-%m-%d"):
-            try:
-                return datetime.strptime(date_str, date_format)
-            except ValueError:
-                pass
-        raise ValueError(f"Formato de data inválido: {date_str}")
+        fetch_data = DolarFetchData().fetch_data(self.date)
 
-    def build_url(self, date: str) -> str:
+        result_dolar = ColDtReferencia.add_dt_referencia(fetch_data, self.date)
+        return result_dolar
+
+class TransformTesouroData:
+    def __init__(self, date: str):
+        self.date = date
+
+    def transform(self) -> list[dict]:
         """
-        Constroi a URL com os parâmetros necessários.
+        Transforma os dados da API Selic em um dicionário.
         """
-        parsed_date = self.parse_date(date).strftime("%m-%d-%Y")
-        params = {
-            "@dataCotacao": f"'{parsed_date}'",
-            "$format": "json"
-        }
+        fetch_data = TesouroFetchData().fetch_data(self.date)
 
-        return f"{self.endpoint}?{urlencode(params)}"
+        result_tesouro = ColDtReferencia.add_dt_referencia(fetch_data, self.date)
+        return result_tesouro
 
-    def fetch_data(self, date: str) -> dict:
-        """
-        Realiza a requisição HTTP e retorna os dados como um DataFrame.
-        """
-        try:
-            url = self.build_url(date)
-            logging.debug(f"URL construída: {url}")
+class FactoryAPIs:
+    @staticmethod
+    def execute_api(api_name: str, params: dict) -> list[dict]:
 
-            # Faz a requisição HTTP ignorando a verificação de certificados
-            r = requests.get(url, timeout=30)
-            r.raise_for_status()
-
-            return r.json().get("value", [])
-
-        except Exception as e:
-            logging.error(f"Erro na API Dolar: {e}")
-            return None
-
-
-# ============== API IBGE ==============
-class IbgeFetchData(IFetchData):
-    def __init__(self):
-        super().__init__(file_yaml["endpoints"]["ibge"]["ipca"]["url"])
-
-    def parse_date(self, date_str: str) -> datetime:
-        """
-        Tenta interpretar a data em diferentes formatos possíveis.
-        Suporta os formatos: dd/mm/yyyy e yyyy-mm-dd.
-        """
-        for date_format in ("%d/%m/%Y", "%Y-%m-%d"):
-            try:
-                return datetime.strptime(date_str, date_format)
-            except ValueError:
-                pass
-        raise ValueError(f"Formato de data inválido: {date_str}")
-
-    def build_url(self, date: str) -> str:
-        """
-        Constroi a URL com os parâmetros necessários.
-        """
-        parsed_date = self.parse_date(date).strftime("%Y%m")
-
-        values = {
-            "t": "1737",
-            "n1": "all",
-            "p": parsed_date
-        }
-
-        params = {
-            "formato": "json"
-        }
-
-        values_formatted = [f"{key}/{value}" for key, value in values.items()]
-
-        return f"{self.endpoint}/{'/'.join(values_formatted)}?{urlencode(params)}"
-
-
-    def fetch_data(self, date: str) -> dict:
-        """
-        Busca os dados da API IBGE.
-        """
-        try:
-            url = self.build_url(date)
-            print(url)
-            logging.debug(f"URL IBGE: {url}")
-
-            # Faz a requisição HTTP
-            r = requests.get(url, timeout=30)
-            r.raise_for_status()
-
-            return r.json()
-
-        except Exception as e:
-            logging.error(f"Erro ao buscar dados: {e}")
-            return None
-
-
-# ============== API Tesouro ==============
-class TesouroFetchData(IFetchData):
-    """Classe para coletar dados de taxas do Tesouro Direto."""
-
-    def __init__(self) -> None:
-        super().__init__(
-            file_yaml["endpoints"]["tesouro"]["taxas_tesouro"]["base_url"]
-        )
-
-    def parse_date(self, date_str: str) -> datetime:
-        """Tenta interpretar a data em diferentes formatos possíveis."""
-        for date_format in ("%d/%m/%Y", "%Y-%m-%d"):
-            try:
-                return datetime.strptime(date_str, date_format)
-            except ValueError:
-                pass
-        raise ValueError(f"Formato de data inválido: {date_str}")
-
-    def build_url(self, date: str) -> str:
-        """Constrói a URL para acessar a API do Tesouro Direto."""
-        parsed_date = self.parse_date(date).strftime("%Y%m%d")
-        return f"{self.endpoint}/{parsed_date}.csv"
-
-    def fetch_data(self, date: str) -> dict:
-        """Realiza a requisição HTTP e retorna os dados como um dicionário."""
-        try:
-            url = self.build_url(date)
-            logging.debug(f"URL construída: {url}")
-
-            r = requests.get(url, timeout=30)
-            r.raise_for_status()
-
-            csv_file = StringIO(r.text)
-            reader = csv.reader(csv_file, delimiter=";")
-
-            headers = [
-                "tp_titulo",
-                "dt_vencimento",
-                "dt_base",
-                "taxa_compra_manha",
-                "taxa_venda_manha",
-                "pu_compra_manha",
-                "pu_venda_manha",
-                "pu_base_manha",
-            ]
-
-            target_date = self.parse_date(date).strftime("%d/%m/%Y")
-            list_of_dicts = [
-                dict(zip(headers, row))
-                for row in reader
-                if len(row) >= len(headers) and row[2] == target_date
-            ]
-
-            return list_of_dicts
-
-        except Exception as e:
-            logging.error(f"Erro na API Tesouro Direto: {e}")
-            return None
+        if api_name == 'selic':
+            return TransformSelicData(params['date']).transform()
+        elif api_name == 'focus':
+            return TransformFocusData(params['indicators'], params['date'], params['temporal_series']).transform()
+        elif api_name == 'dolar':
+            return TransformDolarData(params['date']).transform()
+        elif api_name == 'ibge':
+            return TransformIbgeData(params['date']).transform()            
+        elif api_name == 'tesouro':
+            return TransformTesouroData(params['date']).transform()
+        else:
+            raise ValueError(f"API {api_name} não encontrada")
